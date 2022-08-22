11e323c84cee3aba9701b05be760da42b2b7de77
Juliene-Araujo <julienefjs@gmail.com>
2022-08-22 16:34:38 -0300

    atualização

--- src/data.js
+++ src/data.js
@@ -1,13 +1,9 @@
 
 export const filterByRegion = (pokemons, region) => 
-        pokemons.filter(pokemon => pokemon.generation.name===region)
+pokemons.filter(pokemon => pokemon.generation.name===region)
 
 export const filterByType = (pokemons, type) => 
-        pokemons.filter(pokemon => pokemon.type.includes(type))
-        
-        // const pokemons = []
-        // const pokemonsByRegion= filterByRegion(pokemons, "kanto")
-        // const filteredRegionNType = filterByType(pokemonsByRegion, "grass") 
+pokemons.filter(pokemon => pokemon.type.includes(type))
 
 
 
