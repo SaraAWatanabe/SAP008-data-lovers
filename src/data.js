5c36b1bb64842340758a91902a4403c532736fd0
Juliene-Araujo <julienefjs@gmail.com>
2022-08-23 16:22:27 -0300

    Ordenar por HP

--- src/data.js
+++ src/data.js
@@ -4,6 +4,15 @@ export const filterByRegion = (pokemons, region) =>
 
 export const filterByType = (pokemons, type) => 
         pokemons.filter(pokemon => pokemon.type.includes(type))
+
+export const sortBy = (pokemons, prop) =>{
+        console.log(pokemons)
+        return pokemons.sort((a, b)=> a[prop] - b[prop])
+
+}
+
+
+        
         
       
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { filterByRegion } from "./data.js";
 import { filterByType } from "./data.js";
+import { sortBy } from "./data.js";
 import data from "./data/pokemon/pokemon.js";
 
 //.log(example, data);
@@ -11,19 +12,20 @@ const btnJohto = document.getElementById("button-johto");
 const cards = document.getElementById("cards");
 const everyPokemon = data.pokemon;
 const select = document.getElementById("selectType");
+let selectOrder = document.getElementById("select-cp");
 
 
 //templates dos cards//
 const createCards = listPokemon => {
     let infoPokemon = listPokemon.map((pokemon) =>
-
+    
     `<div class="one-container">
-
-        <div class= "second-container">
-
-            <div class="third-container">
-                <p class= "text-name-number" >
-                 rarity:${pokemon["pokemon-rarity"]}
+    
+    <div class= "second-container">
+    
+    <div class="third-container">
+    <p class= "text-name-number" >
+    rarity:${pokemon["pokemon-rarity"]}
                 <b>${pokemon.num} ${pokemon.name.toUpperCase()}</b>
                  type:${pokemon.type}
                 </p>
@@ -31,37 +33,37 @@ const createCards = listPokemon => {
                 <img class = "image-pokemon" src=${pokemon.img} />
 
                 <p class = "text-pokemon-stats" >max-cp: ${pokemon.stats["max-cp"]} max-hp:${pokemon.stats["max-hp"]} 
-                    height:${pokemon.size.height} weight:${pokemon.size.weight}
+                height:${pokemon.size.height} weight:${pokemon.size.weight}
                 </p>
-            </div>
+                </div>
 
-            <p class = "text-about">
+                <p class = "text-about">
                 About: ${pokemon.about}
-            </p>
-            <p class = "text-egg">egg: ${pokemon.egg}. 
+                </p>
+                <p class = "text-egg">egg: ${pokemon.egg}. 
                 buddy distance km: ${pokemon["buddy-distance-km"]}
-            </p>
+                </p>
             <hr>
             <div>
-                <p class="text-resistant">
-                resistant: ${pokemon.resistant}
-                weaknesses: ${pokemon.weaknesses}
-                </p>
-                <p class="attack-defense-stamina">
-                base attack:${pokemon.stats["base-attack"]}
-                base defense:${pokemon.stats["base-defense"]}
-                base stamina:${pokemon.stats["base-stamina"]}
-                </p>
+            <p class="text-resistant">
+            resistant: ${pokemon.resistant}
+            weaknesses: ${pokemon.weaknesses}
+            </p>
+            <p class="attack-defense-stamina">
+            base attack:${pokemon.stats["base-attack"]}
+            base defense:${pokemon.stats["base-defense"]}
+            base stamina:${pokemon.stats["base-stamina"]}
+            </p>
             </div>
 
+            
+            </div>
+            </div>
+            `)
+            return infoPokemon.join(" ")
+        }
 
-        </div>
-    </div>
-    `)
-    return infoPokemon.join(" ")
-}
-
-let pokemonsToBeShow= []
+        let pokemonsToBeShow= []
 //Botão Região kanto//
 btnKanto.addEventListener("click", function () {
     let result = filterByRegion(everyPokemon, "kanto")
@@ -92,18 +94,15 @@ select.addEventListener("change", function(){
     let pokeGrass = createCards(result)
     cards.innerHTML = pokeGrass;
     console.log (pokeGrass)
-
-
-//     const selectElement = document.querySelector('.ice-cream');
-
-// selectElement.addEventListener('change', (event) => {
-//   const result = document.querySelector('.result');
-//   result.textContent = `You like ${event.target.value}`;
-// })
-
-
-      // const pokemons = []
-        // const pokemonsByRegion= filterByRegion(pokemons, "kanto")
-        // const filteredRegionNType = filterByType(pokemonsByRegion, "grass") 
-    console.log(select.value)
 })
+
+selectOrder.addEventListener("change", function(){
+    let orderCpHp = selectOrder.value
+    console.log(orderCpHp)
+    let result = sortBy(everyPokemon, stats["max-cp"])
+    let pokeCpHp = createCards(result)
+    cards.innerHTML = pokeCpHp;
+    console.log(orderCpHp)
+    console.log(result)
+})
+
