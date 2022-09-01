aaee28df82dd92851d6177c2fbc49df3c51bfd1f
sara <saraakwatanabe@gmail.com>
2022-08-31 22:34:01 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,31 @@
+export const filterByRegion = (pokemons, region) =>
+  pokemons.filter((pokemon) => pokemon.generation.name === region);
+
+export const filterByType = (pokemons, type) =>
+  pokemons.filter((pokemon) => pokemon.type.includes(type));
+
+export const calcPercentage = (pokemonsLength, typeLength) =>
+  (typeLength / pokemonsLength) * 100;
+
+export const filterSearch = (pokemons, name) =>
+  pokemons.filter((pokemon) => pokemon.name.includes(name));
+
+export const sortBy = (pokemons, value) => {
+  //console.log(pokemons, value)
+  return pokemons.sort((a, b) => {
+    if (value == "biggest-max-cp") {
+      return b.stats["max-cp"] - a.stats["max-cp"];
+    }
+    if (value == "smallest-max-cp") {
+      return a.stats["max-cp"] - b.stats["max-cp"];
+    }
+    if (value == "biggest-max-hp") {
+      return b.stats["max-hp"] - a.stats["max-hp"];
+    }
+    if (value == "smallest-max-hp") {
+      return a.stats["max-hp"] - b.stats["max-hp"];
+    }
+    return 0;
+  });
+}
+
