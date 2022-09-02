13212a260b729a6772ea6fe4f8385b718fe19861
Sara A. Watanabe <109092154+SaraAWatanabe@users.noreply.github.com>
2022-09-02 17:07:52 -0300

    Responsividade (#22)
    * nav-ajustada
    
    * responsividade-parcial
    
    * Read-me-inicial

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
+import data from "./data/pokemon/pokemon.js";
 import { filterByRegion } from "./data.js";
 import { filterByType } from "./data.js";
 import { sortBy } from "./data.js";
 import { calcPercentage } from "./data.js";
-import data from "./data/pokemon/pokemon.js";
 import { filterSearch } from "./data.js";
 
-//.log(example, data);
-
+const everyPokemon = data.pokemon;
 const btnReset = document.getElementById("button-reset");
 const btnWorld = document.getElementById("button-world");
 const btnKanto = document.getElementById("button-kanto");
 const btnJohto = document.getElementById("button-johto");
 const cards = document.getElementById("cards");
-const everyPokemon = data.pokemon;
 const select = document.getElementById("selectType");
 let selectOrder = document.getElementById("select-cp");
-const statsType = document.getElementById("statsType")
-const inputSearch = document.getElementById("input-search")
+const statsType = document.getElementById("stats-type");
+const inputSearch = document.getElementById("input-search");
+const inicialDisplay = document.querySelector(".inf-world-pokemon");
+const selects = document.querySelector(".btn-select");
+const search = document.querySelector(".search");
 
+btnReset.addEventListener("click", (event) => {
+  location.reload(event);
+});
 
-
-//botão de reset
-btnReset.addEventListener("click",(event) => {
-    location.reload(event);
-})
-
-//templates dos cards//
-const createCards = listPokemon => {
-    let infoPokemon = listPokemon.map((pokemon) =>
-
-        `<div class="one-container">
+const createCards = (listPokemon) => {
+  let infoPokemon = listPokemon.map(
+    (pokemon) =>
+      `<div class="one-container">
     
     <div class= "second-container">
     
@@ -42,7 +39,9 @@ const createCards = listPokemon => {
 
                 <img class = "image-pokemon" src=${pokemon.img} />
 
-                <p class = "text-pokemon-stats" >max-cp: ${pokemon.stats["max-cp"]} max-hp:${pokemon.stats["max-hp"]} 
+                <p class = "text-pokemon-stats" >max-cp: ${
+                  pokemon.stats["max-cp"]
+                } max-hp:${pokemon.stats["max-hp"]} 
                 height:${pokemon.size.height} weight:${pokemon.size.weight}
                 </p>
                 </div>
@@ -64,95 +63,82 @@ const createCards = listPokemon => {
             base defense:${pokemon.stats["base-defense"]}
             base stamina:${pokemon.stats["base-stamina"]}
             </p>
-            </div>
-
-            
+            </div>           
             </div>
             </div>
-            `)
-    return infoPokemon.join(" ")
-}
+            `
+  );
+  return infoPokemon.join(" ");
+};
 
-
-let pokemonsToBeShown = []
-let sortpokemon = []
-
-//Botão Região kanto//
+let pokemonsToBeShown = [];
+let sortpokemon = [];
 btnKanto.addEventListener("click", function () {
-    document.getElementById("button-kanto").style.background="blue"
-    document.getElementById("button-johto").style.background="white"
-    document.getElementById("button-world").style.background="white"
-    document.querySelector(".inf-world-pokemon").style.display="none"
-    document.querySelector(".btn-select").style.display="block"
-    document.querySelector("#input-search").style.display="inline"
-    let result = filterByRegion(everyPokemon, "kanto")
-    let infoKanto = createCards(result)
-    cards.innerHTML = infoKanto
-    pokemonsToBeShown = result
-    sortpokemon = result
+  btnKanto.style.background = "#FDDDE2";
+  btnJohto.style.background = "white";
+  btnWorld.style.background = "white";
+  inicialDisplay.style.display = "none";
+  selects.style.display = "block";
+  search.style.display = "inline";
+  let result = filterByRegion(everyPokemon, "kanto");
+  let infoKanto = createCards(result);
+  cards.innerHTML = infoKanto;
+  pokemonsToBeShown = result;
+  sortpokemon = result;
 });
 
-//Botão da Região Johto//
 btnJohto.addEventListener("click", function () {
-    document.getElementById("button-johto").style.background="blue"
-    document.getElementById("button-kanto").style.background="white"
-    document.getElementById("button-world").style.background="white"
-    document.querySelector(".inf-world-pokemon").style.display="none"
-    document.querySelector(".btn-select").style.display="block"
-    document.querySelector("#input-search").style.display="inline"
-    let result = filterByRegion(everyPokemon, "johto")
-    let infoJohto = createCards(result)
-    cards.innerHTML = infoJohto;
-    pokemonsToBeShown = result
-    sortpokemon = result
+  btnJohto.style.background = "#FDDDE2";
+  btnKanto.style.background = "white";
+  btnWorld.style.background = "white";
+  inicialDisplay.style.display = "none";
+  selects.style.display = "block";
+  search.style.display = "inline";
+  let result = filterByRegion(everyPokemon, "johto");
+  let infoJohto = createCards(result);
+  cards.innerHTML = infoJohto;
+  pokemonsToBeShown = result;
+  sortpokemon = result;
 });
 
-//Cards de todos os pokemon na tela//
 btnWorld.addEventListener("click", function () {
-    document.getElementById("button-world").style.background="blue"
-    document.getElementById("button-kanto").style.background="white"
-    document.getElementById("button-johto").style.background="white"
-    document.querySelector(".inf-world-pokemon").style.display="none"
-    document.querySelector(".btn-select").style.display="block"
-    document.querySelector("#input-search").style.display="inline"
-    let infoPokemons = createCards(everyPokemon)
-    cards.innerHTML = infoPokemons
-    pokemonsToBeShown = everyPokemon
-    sortpokemon = everyPokemon
+  btnWorld.style.background = "#FDDDE2";
+  btnKanto.style.background = "white";
+  btnJohto.style.background = "white";
+  inicialDisplay.style.display = "none";
+  selects.style.display = "block";
+  search.style.display = "inline";
+  let infoPokemons = createCards(everyPokemon);
+  cards.innerHTML = infoPokemons;
+  pokemonsToBeShown = everyPokemon;
+  sortpokemon = everyPokemon;
 });
-
-//Filtro por tipo//
 
 select.addEventListener("change", function () {
-    const type = select.value
-    const resultType = filterByType(pokemonsToBeShown, type)
-    const pokeType = createCards(resultType)
-    cards.innerHTML = pokeType;
-    sortpokemon = resultType
-    //PROBLEMA: Usar os 251 como parâmetro, e não usar o array por região
-    let typePercentage = calcPercentage(pokemonsToBeShown.length,resultType.length)
-    //statsType.innerHTML = typePercentage + "% dos Pokémon são desse tipo!"
-    //Arredondando
-    statsType.innerHTML = Math.round(typePercentage) + "% dos Pokémon são desse tipo!"
+  const type = select.value;
+  const resultType = filterByType(pokemonsToBeShown, type);
+  const pokeType = createCards(resultType);
+  cards.innerHTML = pokeType;
+  sortpokemon = resultType;
+  let typePercentage = calcPercentage(
+    pokemonsToBeShown.length,
+    resultType.length
+  );
+  
+  statsType.innerHTML =
+    Math.round(typePercentage) + "% dos Pokémon são desse tipo!";
 });
 
-//Ordernar//
 selectOrder.addEventListener("change", function () {
-    let maxCp = selectOrder.value
-    let result = sortBy(sortpokemon, maxCp)
-    let pokeCpHp = createCards(result)
-    cards.innerHTML = pokeCpHp;
-   
-})
+  let maxCp = selectOrder.value;
+  let result = sortBy(sortpokemon, maxCp);
+  let pokeCpHp = createCards(result);
+  cards.innerHTML = pokeCpHp;
+});
 
 inputSearch.addEventListener("input", () => {
-    let text = inputSearch.value
-    let textlowercase = text.toLowerCase()
-    //console.log(text)
-    //console.log(textlowercase)
-    let result = (filterSearch(everyPokemon, textlowercase)); 
-    cards.innerHTML = createCards(result) 
-    //console.log(result)
-  });
-
-  
+  let text = inputSearch.value;
+  let textlowercase = text.toLowerCase();
+  let result = filterSearch(everyPokemon, textlowercase);
+  cards.innerHTML = createCards(result);
+});
