89253d4dcf608a2f9f2c3a191b7f0146e2ab042e
Juliene-Araujo <julienefjs@gmail.com>
2022-09-06 21:20:39 -0300

    alterações de responsividade

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { calcPercentage } from "./data.js";
 import { filterSearch } from "./data.js";
 
 const everyPokemon = data.pokemon;
-const btnReset = document.getElementById("button-reset");
+const btnStart = document.getElementById("button-start");
 const btnWorld = document.getElementById("button-world");
 const btnKanto = document.getElementById("button-kanto");
 const btnJohto = document.getElementById("button-johto");
@@ -18,63 +18,74 @@ const inputSearch = document.getElementById("input-search");
 const inicialDisplay = document.querySelector(".inf-world-pokemon");
 const selects = document.querySelector(".btn-select");
 const search = document.querySelector(".search");
+const btnScreenSecond = document.querySelector(".btnsecond");
 
-btnReset.addEventListener("click", (event) => {
+btnStart.addEventListener("click", (event) => {
   location.reload(event);
 });
 
 const createCards = (listPokemon) => {
   let infoPokemon = listPokemon.map(
-    (pokemon) =>
-      `<div class="one-container">
+    (pokemon) => 
+    `<div class="one-container">
+      <div class= "second-container">
+        <div class="third-container">
+          <p class= "text-name-number" >
+             <b>${pokemon.num} ${pokemon.name.toUpperCase()}</b>
+          </p>
+          <p class="type-rarity"> 
+            type:${pokemon.type} rarity:${pokemon["pokemon-rarity"]}
+          </p>
+          <img class = "image-pokemon" src=${pokemon.img} />
+
+          <p class = "text-pokemon-stats" >
+            max-cp: ${pokemon.stats["max-cp"]} max-hp: ${pokemon.stats["max-hp"]}
+          </p> 
+          <p class = "text-pokemon-stats" >
+            height: ${pokemon.size.height} weight: ${pokemon.size.weight}
+          </p>
+        </div>
+
+        <p class = "text-about">
+          About: ${pokemon.about}
+        </p>
+        <p class = "text-egg">
+          egg: ${pokemon.egg} buddy distance km: ${pokemon["buddy-distance-km"]}
+        </p>
+        <hr>
+        <p class="text-resistant">
+          resistant: ${pokemon.resistant}
+          weaknesses: ${pokemon.weaknesses}
+        </p>
+        <p class="attack-defense-stamina">
+          base attack:${pokemon.stats["base-attack"]}
+          base defense:${pokemon.stats["base-defense"]}
+          base stamina:${pokemon.stats["base-stamina"]}
+        </p>
+      </div>           
     
-    <div class= "second-container">
-    
-    <div class="third-container">
-    <p class= "text-name-number" >
-    rarity:${pokemon["pokemon-rarity"]}
-                <b>${pokemon.num} ${pokemon.name.toUpperCase()}</b>
-                 type:${pokemon.type}
-                </p>
-
-                <img class = "image-pokemon" src=${pokemon.img} />
-
-                <p class = "text-pokemon-stats" >max-cp: ${
-                  pokemon.stats["max-cp"]
-                } max-hp:${pokemon.stats["max-hp"]} 
-                height:${pokemon.size.height} weight:${pokemon.size.weight}
-                </p>
-                </div>
-
-                <p class = "text-about">
-                About: ${pokemon.about}
-                </p>
-                <p class = "text-egg">egg: ${pokemon.egg}. 
-                buddy distance km: ${pokemon["buddy-distance-km"]}
-                </p>
-            <hr>
-            <div>
-            <p class="text-resistant">
-            resistant: ${pokemon.resistant}
-            weaknesses: ${pokemon.weaknesses}
-            </p>
-            <p class="attack-defense-stamina">
-            base attack:${pokemon.stats["base-attack"]}
-            base defense:${pokemon.stats["base-defense"]}
-            base stamina:${pokemon.stats["base-stamina"]}
-            </p>
-            </div>           
-            </div>
-            </div>
-            `
+    </div>`
   );
   return infoPokemon.join(" ");
 };
 
+btnWorld.addEventListener("click", function () {
+  btnWorld.style.background = "rgb(237, 201, 233)";
+  btnKanto.style.background = "white";
+  btnJohto.style.background = "white";
+  inicialDisplay.style.display = "none";
+  selects.style.display = "block";
+  search.style.display = "inline";
+  let infoPokemons = createCards(everyPokemon);
+  cards.innerHTML = infoPokemons;
+  pokemonsToBeShown = everyPokemon;
+  sortpokemon = everyPokemon;
+});
+
 let pokemonsToBeShown = [];
 let sortpokemon = [];
 btnKanto.addEventListener("click", function () {
-  btnKanto.style.background = "#FDDDE2";
+  btnKanto.style.background = "rgb(237, 201, 233)";
   btnJohto.style.background = "white";
   btnWorld.style.background = "white";
   inicialDisplay.style.display = "none";
@@ -88,7 +99,7 @@ btnKanto.addEventListener("click", function () {
 });
 
 btnJohto.addEventListener("click", function () {
-  btnJohto.style.background = "#FDDDE2";
+  btnJohto.style.background = "rgb(237, 201, 233)";
   btnKanto.style.background = "white";
   btnWorld.style.background = "white";
   inicialDisplay.style.display = "none";
@@ -101,18 +112,6 @@ btnJohto.addEventListener("click", function () {
   sortpokemon = result;
 });
 
-btnWorld.addEventListener("click", function () {
-  btnWorld.style.background = "#FDDDE2";
-  btnKanto.style.background = "white";
-  btnJohto.style.background = "white";
-  inicialDisplay.style.display = "none";
-  selects.style.display = "block";
-  search.style.display = "inline";
-  let infoPokemons = createCards(everyPokemon);
-  cards.innerHTML = infoPokemons;
-  pokemonsToBeShown = everyPokemon;
-  sortpokemon = everyPokemon;
-});
 
 select.addEventListener("change", function () {
   const type = select.value;
@@ -124,7 +123,7 @@ select.addEventListener("change", function () {
     pokemonsToBeShown.length,
     resultType.length
   );
-  
+
   statsType.innerHTML =
     Math.round(typePercentage) + "% dos Pokémon são desse tipo!";
 });
---
+++ test/mock.spec.js
@@ -0,0 +1,134 @@
+// export default {mockPokemons, 
+//   mockType, 
+//   mockCP, 
+//   mockEveryPokemon, 
+//   mockTypePercentage, 
+//   mockTypePercentage1,
+//   mockNames
+// }
+
+// const mockPokemons = [
+//   {
+//     generation: {
+//       num: "generation i",
+//       name: "kanto",
+//     },
+//   },
+//   {
+//     generation: {
+//       num: "generation ii",
+//       name: "johto",
+//     },
+//   },
+// ];
+
+// const mockType = [
+//   {
+//     type: ["grass", "poison"],
+//   },
+//   {
+//     type: ["normal"],
+//   },
+//   {
+//     type: ["fire"],
+//   },
+// ];
+
+// const mockCP = [
+//   {
+//     stats: {
+//       "max-cp": "1115",
+//       "max-hp": "113",
+//     },
+//   },
+//   {
+//     stats: {
+//       "max-cp": "2720",
+//       "max-hp": "162",
+//     },
+//   },
+//   {
+//     stats: {
+//       "max-cp": "3265",
+//       "max-hp": "189",
+//     },
+//   },
+//   {
+//     stats: {
+//       "max-cp": "1115",
+//       "max-hp": "113",
+//     },
+//   },
+// ];
+
+// const mockEveryPokemon = [
+//   {
+//     type: ["grass", "poison"],
+//   },
+//   {
+//     type: ["normal"],
+//   },
+//   {
+//     type: ["fire"],
+//   },
+//   {
+//     type: ["grass", "poison"],
+//   },
+//   {
+//     type: ["normal"],
+//   },
+//   {
+//     type: ["grass"],
+//   },
+//   {
+//     type: ["fire"],
+//   },
+//   {
+//     type: ["poison"],
+//   },
+//   {
+//     type: ["dark", "poison"],
+//   },
+//   {
+//     type: ["flying"],
+//   },
+// ];
+
+// const mockTypePercentage = [
+//   {
+//     type: ["grass", "poison"],
+//   },
+//   {
+//     type: ["grass", "poison"],
+//   },
+//   {
+//     type: ["grass"],
+//   },
+// ];
+
+// const mockTypePercentage1 = [
+//   {
+//     type: ["grass", "poison"],
+//   },
+//   {
+//     type: ["grass", "poison"],
+//   },
+// ];
+
+// const mockNames = [
+//   {
+//     name: "pikachu",
+//   },
+//   {
+//     name: "pichu",
+//   },
+//   {
+//     name: "rapidash",
+//   },
+//   {
+//     name: "snorlax",
+//   },
+//   {
+//     name: "articuno",
+//   },
+// ];
